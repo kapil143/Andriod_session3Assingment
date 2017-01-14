303c95a46da2d5670273815fe2dc4130ce6a8f8a
kapil143 <sainikapil.ks@gmail.com>
2017-01-14 11:24:53 +0530

    assingment3.1,3.2,3.3,3.4
    assingment3.1,3.2,3.3,3.4

---
+++ assingment3.3/project/andriodassingment3.3/app/src/main/java/com/example/kapil/andriodassingment33/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.kapil.andriodassingment33;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        File imgFile = new File("/sdcard/Images/test_images.jpg");
+        if (imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        }
+    }
+    public void loadimagefromGallery(View view){
+        Intent myIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(myIntent,RESULT_LOAD_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if(requestCode==RESULT_LOAD_IMAGE && resultCode ==RESULT_OK && null!=data){
+
+                Uri image = data.getData();
+                String[] filePatchToColumn = {MediaStore.Images.Media.DATA};
+
+                Cursor myCursor = getContentResolver().query(image,filePatchToColumn,null,null,null);
+                myCursor.moveToFirst();
+
+                int columnIndex = myCursor.getColumnIndex(filePatchToColumn[0]);
+                String ImgDecode = myCursor.getString(columnIndex);
+                ImageView imgV = (ImageView)findViewById(R.id.viewing);
+                imgV.setImageBitmap(BitmapFactory.decodeFile(ImgDecode));
+
+            }
+            else{
+                Toast.makeText(getBaseContext(),"You Haven't Picked an Image",Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+        catch(Exception e){
+            Toast.makeText(getBaseContext(),"Something Went Wrong", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+}
+
+
---
+++ assingment3.4/project/Assingment3.4/app/src/main/java/com/example/kapil/assingment34/SecondActivity.java
@@ -0,0 +1,7 @@
+package com.example.kapil.assingment34;
+
+/**
+ * Created by kapil on 12-01-2017.
+ */
+public class SecondActivity {
+}
